6bacd005d8c55d8c586117d1f7f24f40567aba9f
LawrenceYep <circle165@gmail.com>
2019-10-12 09:31:00 +0800

    Add files via upload

---
+++ response.js
@@ -0,0 +1,57 @@
+// $request, $response, $notify(title, subtitle, message), console.log(message)
+// $request.scheme, $request.method, $request.url, $request.path, $request.headers
+// $response.statusCode, $response.headers, $response.body
+
+var statusCode = $response.statusCode;
+var headers = $response.headers;
+var body = $response.body;
+
+statusCode = 200;
+$done(statusCode);
+
+var header = JSON.parse(headers);
+var obj = JSON.parse(body);
+header['Access-Control-Allow-Origin'] = '*';
+header['Cache-Control'] = 'private';
+header['Content-Type'] = 'text/html';
+header['charset'] = 'UTF-8';
+headers = JSON.stringify(header);
+$done(headers);
+
+
+obj = {
+	"errorCode": 0,
+	"result": {
+		"service": {
+			"title": "MapleStory M Global",
+			"buildVer": "2",
+			"policyApiVer": "2",
+			"termsApiVer": "2",
+			"useTPA": 0,
+			"loginUIType": "1",
+			"clientId": "MTY3MDg3NDAy",
+			"useMemberships": [101, 103, 102, 9999],
+			"useMembershipsInfo": {
+				"nexonNetSecretKey": "",
+				"nexonNetProductId": "",
+				"nexonNetRedirectUri": ""
+			}
+		},
+		"endBanner": {},
+		"basePlate": [],
+		"country": "HK",
+		"termsAgree": [],
+		"isPrivacyConsigned": -1,
+		"useLocalPolicy": ["0", "0"],
+		"enableLogging": false,
+		"offerwall": {
+			"id": 0,
+			"title": ""
+		}
+	},
+	"errorText": "1",
+	"errorDetail": ""
+};
+body = JSON.stringify(obj);
+$done(body);
+
