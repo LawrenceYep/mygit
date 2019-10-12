122ce9e23fe2ae154a873e893ddd2174cfe30d23
LawrenceYep <circle165@gmail.com>
2019-10-12 11:36:45 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,49 @@
+// $request, $response, $notify(title, subtitle, message), console.log(message)
+// $request.scheme, $request.method, $request.url, $request.path, $request.headers
+// $response.statusCode, $response.headers, $response.body
+
+var headers = $response.headers;
+
+var body = $response.body;
+
+headers['errorCode']=0;
+
+headers = JSON.stringify(headers);
+$done(headers)
+
+body = {
+	"errorCode": 0,
+	"result": {
+		"service": {
+			"title": "MapleStory M Global",
+			"buildVer": "2",
+			"policyApiVer": "2",
+			"termsApiVer": "2",
+			"useTPA": 0,
+			"loginUIType": "1",
+			"clientId": "MTY3MDg3NDAy",
+			"useMemberships": [101, 103, 102, 9999],
+			"useMembershipsInfo": {
+				"nexonNetSecretKey": "",
+				"nexonNetProductId": "",
+				"nexonNetRedirectUri": ""
+			}
+		},
+		"endBanner": {},
+		"basePlate": [],
+		"country": "CN",
+		"termsAgree": [],
+		"isPrivacyConsigned": -1,
+		"useLocalPolicy": ["0", "0"],
+		"enableLogging": false,
+		"offerwall": {
+			"id": 0,
+			"title": ""
+		}
+	},
+	"errorText": "1",
+	"errorDetail": ""
+};
+body = JSON.stringify(body);
+
+$done(body);
