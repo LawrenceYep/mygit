9f3db6244e45cfe62f0659d854a1144ebfd6d08d
LawrenceYep <circle165@gmail.com>
2019-10-18 10:39:45 +0800

    Update response.js

--- response.js
+++ response.js
@@ -2,43 +2,10 @@
 // $request.scheme, $request.method, $request.url, $request.path, $request.headers
 // $response.statusCode, $response.headers, $response.body
 
-var headers = $response.headers;
-headers['errorCode'] = 0;
-var body = $response.body;
-body = {
-	"errorCode": 0,
-	"result": {
-		"service": {
-			"title": "MapleStory M Global",
-			"buildVer": "2",
-			"policyApiVer": "2",
-			"termsApiVer": "2",
-			"useTPA": 0,
-			"loginUIType": "1",
-			"clientId": "MTY3MDg3NDAy",
-			"useMemberships": [101, 103, 102, 9999],
-			"useMembershipsInfo": {
-				"nexonNetSecretKey": "",
-				"nexonNetProductId": "",
-				"nexonNetRedirectUri": ""
-			}
-		},
-		"endBanner": {},
-		"basePlate": [],
-		"country": "CN",
-		"termsAgree": [],
-		"isPrivacyConsigned": -1,
-		"useLocalPolicy": ["0", "0"],
-		"enableLogging": false,
-		"offerwall": {
-			"id": 0,
-			"title": ""
-		}
-	},
-	"errorText": "1",
-	"errorDetail": ""
-};
-body = JSON.stringify(body);
+var header1 = $response.headers;
+header1.errorCode = 0;
+var obj = $response.body;
+obj.errorCode = 0
 
-$done(body);
+$done({body:JSON.stringify(obj),headers:JSON.stringify(header1)});
 
