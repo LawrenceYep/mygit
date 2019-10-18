257227bfd0d6a35852f5c31c6bb27f68976d3d16
LawrenceYep <circle165@gmail.com>
2019-10-18 10:49:30 +0800

    Update response.js

--- response.js
+++ response.js
@@ -2,7 +2,7 @@
 // $request.scheme, $request.method, $request.url, $request.path, $request.headers
 // $response.statusCode, $response.headers, $response.body
 
-var header1 = JSON.parse($response.headers);
+var header1 = $response.headers;
 header1.errorCode = 0;
 var obj = JSON.parse($response.body);
 obj.errorCode = 0
