147c576b854b2eb7a7fe4943972a5575a86109bc
LawrenceYep <circle165@gmail.com>
2019-10-18 10:44:28 +0800

    Update response.js

--- response.js
+++ response.js
@@ -2,9 +2,9 @@
 // $request.scheme, $request.method, $request.url, $request.path, $request.headers
 // $response.statusCode, $response.headers, $response.body
 
-var header1 = $response.headers;
+var header1 = JSON.parse($response.headers);
 header1.errorCode = 0;
-var obj = $response.body;
+var obj = JSON.parse($response.body);
 obj.errorCode = 0
 
 $done({body:JSON.stringify(obj),headers:JSON.stringify(header1)});
