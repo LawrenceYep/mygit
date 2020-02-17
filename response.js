5c963199876177365f804e2c58d8c4e823b89684
LawrenceYep <circle165@gmail.com>
2020-02-17 10:18:29 +0800

    Update response.js

--- response.js
+++ response.js
@@ -2,10 +2,15 @@
 // $request.scheme, $request.method, $request.url, $request.path, $request.headers
 // $response.statusCode, $response.headers, $response.body
 
-var header1 = $response.headers;
-header1.errorCode = 0;
-var obj = JSON.parse($response.body);
-obj.errorCode = 0
 
-$done({body:JSON.stringify(obj),headers:JSON.stringify(header1)});
 
+var modifiedHeaders = $response.headers;
+modifiedHeaders['errorCode'] = 0;
+
+var modifiedStatus = 'HTTP/1.1 200 OK';
+
+var body = $response.body;
+var obj = {"errorCode":0,"result":{"service":{"title":"MapleStory M Global","buildVer":"2","policyApiVer":"2","termsApiVer":"2","useTPA":0,"loginUIType":"1","clientId":"MTY3MDg3NDAy","useMemberships":[101,103,102,9999],"useMembershipsInfo":{"nexonNetSecretKey":"","nexonNetProductId":"","nexonNetRedirectUri":""}},"endBanner":{},"basePlate":[],"country":"CN","termsAgree":[],"isPrivacyConsigned":-1,"useLocalPolicy":["0","0"],"enableLogging":false,"offerwall":{"id":0,"title":""}},"errorText":"1","errorDetail":""}
+body = JSON.stringify(obj);
+
+$done({body: body, headers: modifiedHeaders, status: modifiedStatus})
